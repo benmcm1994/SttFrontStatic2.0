331c8d78c67a4a4a7fac96e6079a8537488d2475
benmcm1994 <mcmullanbenjamin@gmail.com>
2019-08-13 14:52:47 +0100

    Account manager complete

--- source/account-manager.blade.php
+++ source/account-manager.blade.php
@@ -1,10 +1,15 @@
+---
+title: Account Manager
+crawl: true
+---
+
 @extends('_layouts.master')
 
 @section('body')
     <section class="account-intro">
         <div class="container">
             <div class="row">
-                <div class="col-8">
+                <div class="col-12 col-md-9 text-center text-md-left text-justify align-self-center">
                     <p>
                         <strong>Social Trader Tools</strong> allows you to manage an unlimited amount of MT4 accounts from <strong>any MT4 broker</strong>,
                         all from one platform. There is no special software, no complex configurations and <strong>no VPS servers required</strong>.
@@ -14,19 +19,86 @@
                         Once you have accounts added you can utilise the platform in many ways.
                     </p>
                 </div>
-                <div class="col-4"><img src="/assets/images/responsive.png" alt=""></div>
+                <div class="col-12 col-md-3 text-center"><img src="/assets/images/responsive.png" alt=""></div>
             </div>
         </div>
-
-        <div class="containerx">
-            <div class="item item1"></div>
-            <div class="item item2"></div>
-            <div class="item item3"></div>
-            <div class="item item4"></div>
-            <div class="item item5"></div>
-            <div class="item item6"></div>
-            <div class="item item7"></div>
+    </section>
+    <section class="account-features">
+        <div class="container">
+            <div class="row">
+                <div class="col-12"><h2 class="section-header text-center">Key Features</h2></div>
+            </div>
+            <div class="row">
+                <div class="col-12 col-md-6">
+                    <div class="row">
+                        <div class="col-3 col-md-2">
+                            <i class="fa fa-desktop fa-3x" aria-hidden="true"></i>
+                        </div>
+                        <div class="col-9 col-md-10">
+                            <h5><strong>The Dashboard</strong></h5>
+                            <p>The Dashboard gives you a real time view of your accounts all in one place,
+                                removing the need to cycle through multiple terminals to manage your portfolio.
+                                You can view performance, exposure, open trades and trade history of accounts overall,
+                                or filtered down by account or specific groups.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6">
+                    <div class="row">
+                        <div class="col-3 col-md-2">
+                            <i class="fa fa-exchange fa-3x" aria-hidden="true"></i>
+                        </div>
+                        <div class="col-9 col-md-10">
+                            <h5><strong>Signal Providing</strong></h5>
+                            <p>Offer out your trading system <a href="https://www.socialtradertools.com/signal-provider/">as a signal</a> to other users on the platform or via <a href="https://www.socialtradertools.com/whitelabel/">your own
+                                    personalised whitelabel</a>. You control what rights the follower has to your signal, and
+                                signals can be offered free or with a cost attached.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr>
+            <div class="row">
+                <div class="col-12 col-md-6">
+                    <div class="row">
+                        <div class="col-3 col-md-2">
+                            <i class="fa fa-share-alt fa-3x" aria-hidden="true"></i>
+                        </div>
+                        <div class="col-9 col-md-10">
+                            <h5><strong>Trade Copying</strong></h5>
+                            <p>Setup <a href="https://www.socialtradertools.com/trade-copier/">trade copiers</a> to copy trades between your own accounts or from signal accounts.
+                                Trade copiers can be setup in a few clicks and settings can be updated at any time.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6">
+                    <div class="row">
+                        <div class="col-3 col-md-2">
+                            <i class="fa fa-envelope fa-3x" aria-hidden="true"></i>
+                        </div>
+                        <div class="col-9 col-md-10">
+                            <h5><strong>Real Time Email Alerts</strong></h5>
+                            <p>Trading a 24 hour market means you need to stay up to date with your activity and exposure.
+                                With <strong>Social Trader Tools</strong>, you can setup real time email alerts for any trading activity, all with a tick of a box.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr>
+            <div class="row">
+                <div class="col-12 col-md-6 offset-md-3">
+                    <div class="row">
+                        <div class="col-3 col-md-2">
+                            <i class="fa fa-shield fa-3x" aria-hidden="true"></i>
+                        </div>
+                        <div class="col-9 col-md-10">
+                            <h5><strong>Account Protection</strong></h5>
+                            <p>In case the worst happens, <strong>Social Trader Tools</strong> can monitor your accounts equity levels.
+                                Whether you want to be alerted or have trades fully closed out, <strong>Social Trader Tools</strong> makes it simple.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-
     </section>
 @endsection
